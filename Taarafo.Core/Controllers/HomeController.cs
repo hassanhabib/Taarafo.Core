f16e60de57d9430868804a606c72fde75394a9b0
Hassan Habib <hassanhabib@live.com>
2021-04-22 17:40:33 -0700

    CONTROLLERS: Home Controller

---
+++ Taarafo.Core/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Taarafo.Core.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Thank you Mario! But the princess is in another castle!");
+    }
+}
