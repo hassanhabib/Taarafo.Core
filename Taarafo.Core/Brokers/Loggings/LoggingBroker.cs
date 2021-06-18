d1d2eb18a1ce146fa6a13bacba72a20e3301fa13
Hassan Habib <harezk@microsoft.com>
2021-06-18 15:52:00 -0700

    BROKERS: Logging Broker

---
+++ Taarafo.Core/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,37 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace Taarafo.Core.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception, exception.Message);
+
+        public void LogDebug(string message) =>
+            this.logger.LogDebug(message);
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception.Message, exception);
+
+        public void LogInformation(string message) =>
+            this.logger.LogInformation(message);
+
+        public void LogTrace(string message) =>
+            this.logger.LogTrace(message);
+
+        public void LogWarning(string message) =>
+            this.logger.LogWarning(message);
+    }
+}
