6cd601f0203d6ca98e690201ab301ee75119dcb4
Hassan Habib <harezk@microsoft.com>
2021-06-18 15:21:56 -0700

    BROKERS: Post Model & Migrations

---
+++ Taarafo.Core/Brokers/Storages/StorageBroker.Posts.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using Taarafo.Core.Models.Posts;
+
+namespace Taarafo.Core.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
