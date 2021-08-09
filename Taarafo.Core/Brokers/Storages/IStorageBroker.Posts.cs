1d99092811cc8de57353fc684f1b0834b885654b
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-08-09 23:34:17 +0200

    Add Crud to operations to Posts Storage interface

--- Taarafo.Core/Brokers/Storages/IStorageBroker.Posts.cs
+++ Taarafo.Core/Brokers/Storages/IStorageBroker.Posts.cs
@@ -3,13 +3,22 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Taarafo.Core.Models.Posts;
 
 namespace Taarafo.Core.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Post> InsertPostAsync(Post post);
+        ValueTask<Post> UpdatePostAsync(Post post);
+        ValueTask<Post> SelectPostByIdAsync(Guid postId);
         IQueryable<Post> SelectAllPosts();
+        ValueTask<Post> DeletePostAsync(Post post);
+
+
+
     }
 }
