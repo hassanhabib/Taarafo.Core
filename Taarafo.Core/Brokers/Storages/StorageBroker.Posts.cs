a31ab8cea8ea8d7890844f0e4cdaacb2a63cd7f1
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-08-09 23:45:29 +0200

    Implement Crud operation to post storage broker with functionality

--- Taarafo.Core/Brokers/Storages/StorageBroker.Posts.cs
+++ Taarafo.Core/Brokers/Storages/StorageBroker.Posts.cs
@@ -3,8 +3,11 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Taarafo.Core.Models.Posts;
 
 namespace Taarafo.Core.Brokers.Storages
@@ -13,6 +16,42 @@ namespace Taarafo.Core.Brokers.Storages
     {
         public DbSet<Post> Posts { get; set; }
 
+        public async ValueTask<Post> InsertPostAsync(Post post)
+        {
+            EntityEntry<Post> postEntityEntry = await this.Posts.AddAsync(post);
+
+            await this.SaveChangesAsync();
+
+            return postEntityEntry.Entity;
+
+        }
+
+        public async ValueTask<Post> UpdatePostAsync(Post post)
+        {
+            EntityEntry<Post> postEntityEntry = this.Posts.Update(post);
+
+            await this.SaveChangesAsync();
+
+            return postEntityEntry.Entity;
+        }
+
+        public async ValueTask<Post> SelectPostByIdAsync(Guid postId)
+        {
+            this.ChangeTracker.QueryTrackingBehavior =
+                QueryTrackingBehavior.NoTracking;
+
+            return await Posts.FindAsync(postId);
+        }
+
         public IQueryable<Post> SelectAllPosts() => this.Posts;
+        public async ValueTask<Post> DeletePostAsync(Post post)
+        {
+            EntityEntry<Post> postEntityEntry = this.Posts.Remove(post);
+
+            await this.SaveChangesAsync();
+
+            return postEntityEntry.Entity;
+
+        }
     }
 }
