31a479be0d7b4f1e3a8257326049c08bf71f77ad
Hassan Habib <harezk@microsoft.com>
2021-10-12 21:39:11 -0700

    ShouldThrowCriticalDepdnencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.Add.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Taarafo.Core.Models.Posts;
+using Taarafo.Core.Models.Posts.Exceptions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.Posts
+{
+    public partial class PostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDepdnencyExceptionOnAddIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Post somePost = CreateRandomPost();
+            SqlException sqlException = GetSqlException();
+
+            var failedPostStorageException =
+                new FailedPostStorageException(sqlException);
+
+            var expectedPostDependencyException =
+                new PostDependencyException(failedPostStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertPostAsync(It.IsAny<Post>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Post> addPostTask =
+                this.postService.AddPostAsync(somePost);
+
+            // then
+            await Assert.ThrowsAsync<PostDependencyException>(() =>
+               addPostTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertPostAsync(It.IsAny<Post>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedPostDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Taarafo.Core/Models/Posts/Exceptions/FailedPostStorageException.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+
+using System;
+using Xeptions;
+
+namespace Taarafo.Core.Models.Posts.Exceptions
+{
+    public class FailedPostStorageException : Xeption
+    {
+        public FailedPostStorageException(Exception innerException)
+            : base ("Failed post storage error occurred, contact support.", innerException)
+        {}
+    }
+}
