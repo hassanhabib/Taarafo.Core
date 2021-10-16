a21627af616455aba794bb06735959334eb4b2ec
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-10-16 21:00:49 +0200

    ShouldAddPostAsync  -> PASS

--- Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Logic.Add.cs
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Logic.Add.cs
@@ -3,6 +3,7 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Force.DeepCloner;
@@ -18,10 +19,15 @@ namespace Taarafo.Core.Tests.Unit.Services.Foundations.Posts
         public async Task ShouldAddPostAsync()
         {
             // given
-            Post randomPost = CreateRandomPost();
+            DateTimeOffset dateTime = GetRadnomDateTimeOffset();
+            Post randomPost = CreateRandomPost(dateTime);
             Post inputPost = randomPost;
             Post storagePost = inputPost;
             Post expectedPost = storagePost.DeepClone();
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Returns(dateTime);
 
             this.storageBrokerMock.Setup(broker =>
                 broker.InsertPostAsync(inputPost))
@@ -34,10 +40,15 @@ namespace Taarafo.Core.Tests.Unit.Services.Foundations.Posts
             // then
             actualPost.Should().BeEquivalentTo(expectedPost);
 
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(), 
+                    Times.Once);
+
             this.storageBrokerMock.Verify(broker =>
                 broker.InsertPostAsync(inputPost),
                     Times.Once);
 
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
