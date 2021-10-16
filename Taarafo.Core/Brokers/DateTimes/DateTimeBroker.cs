834f9877a9727a9f1723498785186d1d1104787f
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-10-16 06:40:52 +0200

    BROKERS: DateTime Broker

---
+++ Taarafo.Core/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+
+namespace Taarafo.Core.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTimeOffset() =>
+            DateTimeOffset.UtcNow;   
+    }
+}
