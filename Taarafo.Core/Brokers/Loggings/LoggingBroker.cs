c370a80e1e0d7fcbe8fb5c11961ae86504357dab
Wellington Camargo <wellington_balbo_lp@hotmail.com>
2021-10-16 23:49:14 -0300

    CODE RUB: Cleaned Logging Broker.

--- Taarafo.Core/Brokers/Loggings/LoggingBroker.cs
+++ Taarafo.Core/Brokers/Loggings/LoggingBroker.cs
@@ -3,7 +3,6 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
-
 using System;
 using Microsoft.Extensions.Logging;
 
@@ -16,22 +15,22 @@ namespace Taarafo.Core.Brokers.Loggings
         public LoggingBroker(ILogger<LoggingBroker> logger) =>
             this.logger = logger;
 
-        public void LogCritical(Exception exception) =>
-            this.logger.LogCritical(exception, exception.Message);
-
-        public void LogDebug(string message) =>
-            this.logger.LogDebug(message);
-
-        public void LogError(Exception exception) =>
-            this.logger.LogError(exception.Message, exception);
-
         public void LogInformation(string message) =>
             this.logger.LogInformation(message);
 
         public void LogTrace(string message) =>
             this.logger.LogTrace(message);
 
+        public void LogDebug(string message) =>
+            this.logger.LogDebug(message);
+
         public void LogWarning(string message) =>
             this.logger.LogWarning(message);
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception.Message, exception);
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception, exception.Message);
     }
 }
