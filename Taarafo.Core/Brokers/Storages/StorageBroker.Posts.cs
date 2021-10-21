447e01fdc6f6c50f4b3ad4445ccba7567185a616
Ricky <rcruz78@outlook.com>
2021-10-20 22:29:10 -0400

    BROKERS: Delete and Select post

--- Taarafo.Core/Brokers/Storages/StorageBroker.Posts.cs
+++ Taarafo.Core/Brokers/Storages/StorageBroker.Posts.cs
@@ -3,10 +3,11 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Taarafo.Core.Models.Posts;
 
 namespace Taarafo.Core.Brokers.Storages
@@ -34,6 +35,27 @@ namespace Taarafo.Core.Brokers.Storages
                 new StorageBroker(this.configuration);
 
             return broker.Posts;
+        }
+
+        public async ValueTask<Post> SelectPostByIdAsync(Guid postId)
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            return await broker.Posts.FindAsync(postId);
+        }
+
+        public async ValueTask<Post> DeletePostAsync(Post post)
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            EntityEntry<Post> postEntityEntry =
+                broker.Posts.Remove(post);
+
+            await broker.SaveChangesAsync();
+
+            return postEntityEntry.Entity;
         }
     }
 }
