ac0a5f43c42170fa04dcde620aed431576b6e9dd
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-10-21 08:55:44 +0200

    ShouldRetrievePostById -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Taarafo.Core.Models.Posts;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.Posts
+{
+    public partial class PostServiceTests
+    {
+        [Fact]
+        public void ShouldRetrievePostById()
+        {
+            // given
+            Post randomPosts = CreateRandomPost();
+            Guid inputPostId = Guid.NewGuid();
+            Post storagePosts = randomPosts;
+            Post expectedPosts = storagePosts.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectPostByIdAsync(inputPostId))
+                    .ReturnsAsync(storagePosts);
+
+            // when
+            ValueTask<Post> actualPosts =
+                this.postService.RetrievePostById (inputPostId);
+
+            // then
+            actualPosts.Should().BeEquivalentTo(expectedPosts);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllPosts(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- Taarafo.Core/Services/Foundations/Posts/IPostService.cs
+++ Taarafo.Core/Services/Foundations/Posts/IPostService.cs
@@ -3,6 +3,7 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Taarafo.Core.Models.Posts;
@@ -13,5 +14,6 @@ namespace Taarafo.Core.Services.Foundations.Posts
     {
         ValueTask<Post> AddPostAsync(Post post);
         IQueryable<Post> RetrieveAllPosts();
+        ValueTask<Post> RetrievePostById(Guid postId);
     }
 }
