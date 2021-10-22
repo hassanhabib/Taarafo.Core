531c86867d070489f584af5a798a144fa0ab761e
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-10-22 21:41:23 +0200

    ShouldThrowCriticalDependencyExceptinOnRemoveByIdIfSqlErrorOccursAndLogItAsync -> PASS

--- Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.RemoveById.cs
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.RemoveById.cs
@@ -16,6 +16,49 @@ namespace Taarafo.Core.Tests.Unit.Services.Foundations.Posts
     public partial class PostServiceTests
     {
         [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptinOnRemoveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            var randomPostId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedPostStorageException =
+                new FailedPostStorageException(sqlException);
+
+            var expectedPostDependencyException =
+                new PostDependencyException(failedPostStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectPostByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+            // when
+            ValueTask<Post> removePostByIdTask =
+                this.postService.RemovePostByIdAsync(randomPostId);
+
+            // then
+            await Assert.ThrowsAsync<PostDependencyException>(() =>
+                removePostByIdTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPostByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedPostDependencyException))),
+                    Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeletePostAsync(It.IsAny<Post>()),
+                    Times.Never());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+
+        [Fact]
         public async Task ShouldThrowServiceExceptionOnRemoveByIdIfServiceErrorOccursAndLogItAsync()
         {
             // given
