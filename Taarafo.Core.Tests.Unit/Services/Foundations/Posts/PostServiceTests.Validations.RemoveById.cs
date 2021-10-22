27704ad045c29c8a2d92b4667b4e999d041c379c
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-10-22 20:04:31 +0200

    ShouldThrowValidationExceptionOnRemoveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Validations.RemoveById.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using Taarafo.Core.Models.Posts;
+using Taarafo.Core.Models.Posts.Exceptions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.Posts
+{
+    public partial class PostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidPostId = Guid.Empty;
+
+            var invalidPostException =
+                new InvalidPostException();
+
+            invalidPostException.AddData(
+                key: nameof(Post.Id),
+                values: "Id is required");
+
+            var expectedPostValidationException = new
+                PostValidationException(invalidPostException);
+
+            // when
+            ValueTask<Post> removePostByIdTask = 
+                this.postService.RemovePostByIdAsync(invalidPostId);
+
+            // then
+            await Assert.ThrowsAsync<PostValidationException>(() =>
+                removePostByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostValidationException))),
+                        Times.Once);
+                
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPostByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeletePostAsync(It.IsAny<Post>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
