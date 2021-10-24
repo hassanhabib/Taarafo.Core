225b4f28adcbcfdf518b2b83b30869402f8ef061
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-10-24 21:19:22 +0200

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using Taarafo.Core.Models.Posts;
+using Taarafo.Core.Models.Posts.Exceptions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.Posts
+{
+    public partial class PostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidPostId = Guid.Empty;
+
+            var invalidPostException =
+                new InvalidPostException();
+
+            invalidPostException.AddData(
+                key: nameof(Post.Id),
+                values: "Id is required");
+
+            var expectedPostValidationException = new
+                PostValidationException(invalidPostException);
+
+            // when
+            ValueTask<Post> retrievePostByIdTask =
+                this.postService.RetrievePostByIdAsync(invalidPostId);
+
+            // then
+            await Assert.ThrowsAsync<PostValidationException>(() =>
+                retrievePostByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPostByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
