311ea268787c5537ef2c533dce02e96087d24450
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-10-24 21:45:56 +0200

    ShouldThrowServiceExceptionOnRetrieveByIdIfDatabaseUpdateErrorOccursAndLogItAsync -> PASS

--- Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.RetrieveById.cs
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.RetrieveById.cs
@@ -49,5 +49,43 @@ namespace Taarafo.Core.Tests.Unit.Services.Foundations.Posts
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
 
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveByIdIfDatabaseUpdateErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            var serviceException = new Exception();
+
+            var failedPostServiceException =
+                new FailedPostServiceException(serviceException);
+
+            var expectedPostServiceException =
+                new PostServiceException(failedPostServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectPostByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<Post> retrievePostByIdTask =
+                this.postService.RetrievePostByIdAsync(someId);
+
+            // then
+            await Assert.ThrowsAsync<PostServiceException>(() =>
+                retrievePostByIdTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPostByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedPostServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
