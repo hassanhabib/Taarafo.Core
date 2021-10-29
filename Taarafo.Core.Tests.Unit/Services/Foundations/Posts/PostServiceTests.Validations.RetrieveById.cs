e88a0d445933c9736f5a37af63df493479280d06
Hassan Habib <hassanhabib@live.com>
2021-10-28 19:21:48 -0700

    ShouldThrowNotFoundExceptionIfPostIsNotFoundAndLogItAsync -> FAIL

--- Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Validations.RetrieveById.cs
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Validations.RetrieveById.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Moq;
 using Taarafo.Core.Models.Posts;
---
+++ Taarafo.Core/Models/Posts/Exceptions/NotFoundPostException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace Taarafo.Core.Models.Posts.Exceptions
+{
+    public class NotFoundPostException : Xeption
+    {
+        public NotFoundPostException(Guid id)
+            : base($"Could not find post with id: {id}")
+        { }
+    }
+}
