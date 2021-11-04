d5cf6415fd424f1b04e462aa7124652ccc49cb7b
Christo du Toit <christo@dutoit.co.uk>
2021-11-04 23:14:38 +0000

    ShouldThrowValidationExceptionOnAddIfCommentIsInvalidAndLogItAsync -> FAIL

--- Taarafo.Core.Tests.Unit/Services/Foundations/Comments/CommentServiceTests.Validations.Add.cs
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Comments/CommentServiceTests.Validations.Add.cs
@@ -38,5 +38,61 @@ namespace Taarafo.Core.Tests.Unit.Services.Foundations.Comments
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfCommentIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidComment = new Comment
+            {
+                Content = invalidText
+            };
+
+            var invalidCommentException =
+                new InvalidCommentException();
+
+            invalidCommentException.AddData(
+                key: nameof(Comment.Id),
+                values: "Id is required");
+
+            invalidCommentException.AddData(
+                key: nameof(Comment.Content),
+                values: "Text is required");
+
+            invalidCommentException.AddData(
+                key: nameof(Comment.CreatedDate),
+                values: "Date is required");
+
+            invalidCommentException.AddData(
+                key: nameof(Comment.UpdatedDate),
+                values: "Date is required");
+
+            var expectedCommentValidationException =
+                new CommentValidationException(invalidCommentException);
+
+            // when
+            ValueTask<Comment> addCommentTask =
+                this.commentService.AddCommentAsync(invalidComment);
+
+            // then
+            await Assert.ThrowsAsync<CommentValidationException>(() =>
+               addCommentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameValidationExceptionAs(
+                    expectedCommentValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertCommentAsync(It.IsAny<Comment>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
