893efff08b32d0868fe5a86c45d8a753108371e0
Christo du Toit <christo@dutoit.co.uk>
2021-11-04 20:59:47 +0000

    BROKERS:  Update Comment

--- Taarafo.Core/Brokers/Storages/StorageBroker.Comments.cs
+++ Taarafo.Core/Brokers/Storages/StorageBroker.Comments.cs
@@ -4,6 +4,8 @@
 // ---------------------------------------------------------------
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Threading.Tasks;
 using Taarafo.Core.Models.Comments;
 
 namespace Taarafo.Core.Brokers.Storages
@@ -11,5 +13,18 @@ namespace Taarafo.Core.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Comment> Comments { get; set; }
+
+        public async ValueTask<Comment> UpdateCommentAsync(Comment comment)
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            EntityEntry<Comment> commentEntityEntry =
+                broker.Comments.Update(comment);
+
+            await broker.SaveChangesAsync();
+
+            return commentEntityEntry.Entity;
+        }
     }
 }
