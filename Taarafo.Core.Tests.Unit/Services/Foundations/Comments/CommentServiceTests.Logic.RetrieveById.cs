92faabcf842c9cfeddeedd4de07828955963584e
Christo du Toit <christo@dutoit.co.uk>
2021-11-08 12:28:40 +0000

    ShouldRetrieveCommentByIdAsync -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Comments/CommentServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,48 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Taarafo.Core.Models.Comments;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.Comments
+{
+    public partial class CommentServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveCommentByIdAsync()
+        {
+            // given
+            Guid randomCommentId = Guid.NewGuid();
+            Guid inputCommentId = randomCommentId;
+            Comment randomComment = CreateRandomComment();
+            Comment storageComment = randomComment;
+            Comment expectedComment = storageComment.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectCommentByIdAsync(inputCommentId))
+                    .ReturnsAsync(storageComment);
+
+            // when
+            Comment actualComment =
+                await this.commentService.RetrieveCommentByIdAsync(inputCommentId);
+
+            // then
+            actualComment.Should().BeEquivalentTo(expectedComment);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCommentByIdAsync(inputCommentId),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- Taarafo.Core/Services/Foundations/Comments/CommentService.cs
+++ Taarafo.Core/Services/Foundations/Comments/CommentService.cs
@@ -3,6 +3,7 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using Taarafo.Core.Brokers.DateTimes;
 using Taarafo.Core.Brokers.Loggings;
@@ -34,5 +35,7 @@ namespace Taarafo.Core.Services.Foundations.Comments
 
             return await this.storageBroker.InsertCommentAsync(comment);
         });
+
+        public ValueTask<Comment> RetrieveCommentByIdAsync(Guid commentId) => throw new NotImplementedException();
     }
 }
