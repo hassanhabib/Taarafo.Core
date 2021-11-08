17ade9890393861010b3f88801024b661adb9f92
Christo du Toit <christo@dutoit.co.uk>
2021-11-08 13:52:03 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Comments/CommentServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Taarafo.Core.Models.Comments.Exceptions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.Comments
+{
+    public partial class CommentServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedStorageException =
+                new FailedCommentStorageException(sqlException);
+
+            var expectedCommentDependencyException =
+                new CommentDependencyException(failedStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllComments())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllCommentsAction = () =>
+                this.commentService.RetrieveAllComments();
+
+            // then
+            Assert.Throws<CommentDependencyException>(
+                retrieveAllCommentsAction);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllComments(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedCommentDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
