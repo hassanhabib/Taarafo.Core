62cfbef2e6e1dffa232cd8b235ed6feda266d9b6
Christo du Toit <christo@dutoit.co.uk>
2021-11-09 23:39:14 +0000

    ShouldThrowValidationExceptionOnModifyIfCommentIsInvalidAndLogItAsync -> PASS

--- Taarafo.Core.Tests.Unit/Services/Foundations/Comments/CommentServiceTests.Validation.Modify.cs
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Comments/CommentServiceTests.Validation.Modify.cs
@@ -3,10 +3,8 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
-using System;
-using System.Threading.Tasks;
-using Force.DeepCloner;
 using Moq;
+using System.Threading.Tasks;
 using Taarafo.Core.Models.Comments;
 using Taarafo.Core.Models.Comments.Exceptions;
 using Xunit;
@@ -37,6 +35,73 @@ namespace Taarafo.Core.Tests.Unit.Services.Foundations.Comments
                 broker.LogError(It.Is(SameValidationExceptionAs(
                     expectedCommentValidationException))),
                         Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateCommentAsync(It.IsAny<Comment>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfCommentIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidComment = new Comment
+            {
+                Content = invalidText,
+            };
+
+            var invalidCommentException = new InvalidCommentException();
+
+            invalidCommentException.AddData(
+                key: nameof(Comment.Id),
+                values: "Id is required");
+
+            invalidCommentException.AddData(
+                key: nameof(Comment.Content),
+                values: "Text is required");
+
+            invalidCommentException.AddData(
+                key: nameof(Comment.CreatedDate),
+                values: "Date is required");
+
+            invalidCommentException.AddData(
+                key: nameof(Comment.UpdatedDate),
+                values:
+                new[] {
+                    "Date is required",
+                    $"Date is the same as {nameof(Comment.CreatedDate)}"
+                });
+
+            invalidCommentException.AddData(
+                key: nameof(Comment.PostId),
+                values: "Id is required");
+
+            var expectedCommentValidationException =
+                new CommentValidationException(invalidCommentException);
+
+            // when
+            ValueTask<Comment> modifyCommentTask =
+                this.commentService.ModifyCommentAsync(invalidComment);
+
+            //then
+            await Assert.ThrowsAsync<CommentValidationException>(() =>
+                modifyCommentTask.AsTask());
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameValidationExceptionAs(
+                    expectedCommentValidationException))),
+                        Times.Once());
 
             this.storageBrokerMock.Verify(broker =>
                 broker.UpdateCommentAsync(It.IsAny<Comment>()),
