7134d728a67deaeedea0dc1da07139aad839f0aa
Stanislaw Nowak <microsoft@stas.email>
2021-11-18 01:57:41 +0100

    ShouldPostPostAsync => FAIL

---
+++ Taarafo.Core.Tests.Acceptance/Brokers/ApiBroker.Posts.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Taarafo.Core.Tests.Acceptance.Models.Posts;
+
+namespace Taarafo.Core.Tests.Acceptance.Brokers
+{
+    public partial class ApiBroker
+    {
+        public ValueTask<Post> PostPostAsync(Post post) =>
+            throw new NotImplementedException();
+
+        public async ValueTask<Post> GetPostByIdAsync(Guid postId) =>
+            throw new NotImplementedException();
+
+        public async ValueTask<Post> DeletePostByIdAsync(Guid postId) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ Taarafo.Core.Tests.Acceptance/Models/Posts/Post.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+
+namespace Taarafo.Core.Tests.Acceptance.Models.Posts
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public Guid Author { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
