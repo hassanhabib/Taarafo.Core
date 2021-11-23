69c5a9c5945eb510be2bd021b62701ce63c8c22f
Christo du Toit <christo@dutoit.co.uk>
2021-11-23 19:00:51 +0000

    PROVISION: Initialize Cloud Broker

---
+++ Taarafo.Core.Infrastructure.Provision/Program.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+namespace Taarafo.Core.Infrastructure.Provision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
