d78d28f46e141c9df27780792bc919e1e3b2062c
Christo du Toit <christo@dutoit.co.uk>
2021-11-26 16:40:17 +0000

    FOUNDATIONS: Implement Provisioning Resource Group

---
+++ Taarafo.Core.Infrastructure.Provision/Services/Foundations/CloudManagements/CloudManagementService.cs
@@ -0,0 +1,42 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Taarafo.Core.Infrastructure.Provision.Brokers.Clouds;
+using Taarafo.Core.Infrastructure.Provision.Brokers.Loggings;
+
+namespace Taarafo.Core.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public class CloudManagementService : ICloudManagementService
+    {
+        private readonly ICloudBroker cloudBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public CloudManagementService(
+            ICloudBroker cloudBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.cloudBroker = new CloudBroker();
+            this.loggingBroker = new LoggingBroker();
+        }
+
+        public async ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
+            string projectName,
+            string environment)
+        {
+            string resourceGroupName = $"{projectName}-RESOURCES-{environment}".ToUpper();
+            this.loggingBroker.LogActivity(message: $"Provisioning {resourceGroupName}...");
+
+            IResourceGroup resourceGroup =
+                await this.cloudBroker.CreateResourceGroupAsync(
+                    resourceGroupName);
+
+            this.loggingBroker.LogActivity(message: $"{resourceGroupName} Provisioned.");
+
+            return resourceGroup;
+        }
+    }
+}
---
+++ Taarafo.Core.Infrastructure.Provision/Services/Foundations/CloudManagements/ICloudManagementService.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace Taarafo.Core.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public interface ICloudManagementService
+    {
+        ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
+            string projectName,
+            string environment);
+    }
+}
