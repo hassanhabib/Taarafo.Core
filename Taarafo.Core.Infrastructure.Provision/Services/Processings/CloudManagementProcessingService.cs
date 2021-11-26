2efcedf245151b7a253ec2b42a63340abccb4320
Christo du Toit <christo@dutoit.co.uk>
2021-11-26 17:02:03 +0000

    PROCESSINGS: Processing Service to Provision

---
+++ Taarafo.Core.Infrastructure.Provision/Services/Processings/CloudManagementProcessingService.cs
@@ -0,0 +1,83 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.Sql.Fluent;
+using Taarafo.Core.Infrastructure.Provision.Brokers.Configurations;
+using Taarafo.Core.Infrastructure.Provision.Models.Configurations;
+using Taarafo.Core.Infrastructure.Provision.Models.Storages;
+using Taarafo.Core.Infrastructure.Provision.Services.Foundations.CloudManagements;
+
+namespace Taarafo.Core.Infrastructure.Provision.Services.Processings
+{
+    public class CloudManagementProcessingService : ICloudManagementProcessingService
+    {
+        private readonly ICloudManagementService cloudManagementService;
+        private readonly IConfigurationBroker configurationBroker;
+
+        public CloudManagementProcessingService()
+        {
+            this.cloudManagementService = new CloudManagementService();
+            this.configurationBroker = new ConfigurationBroker();
+        }
+
+        public async ValueTask ProcessAsync()
+        {
+            CloudManagementConfiguration cloudManagementConfiguration =
+                this.configurationBroker.GetConfigurations();
+
+            await ProvisionAsync(
+                projectName: cloudManagementConfiguration.ProjectName,
+                cloudAction: cloudManagementConfiguration.Up);
+        }
+
+        private async ValueTask ProvisionAsync(
+            string projectName,
+            CloudAction cloudAction)
+        {
+            List<string> environments = RetrieveEnvironments(cloudAction);
+
+            foreach (string environmentName in environments)
+            {
+                IResourceGroup resourceGroup = await this.cloudManagementService
+                    .ProvisionResourceGroupAsync(
+                        projectName,
+                        environmentName);
+
+                IAppServicePlan appServicePlan = await this.cloudManagementService
+                    .ProvisionPlanAsync(
+                        projectName,
+                        environmentName,
+                        resourceGroup);
+
+                ISqlServer sqlServer = await this.cloudManagementService
+                    .ProvisionSqlServerAsync(
+                        projectName,
+                        environmentName,
+                        resourceGroup);
+
+                SqlDatabase sqlDatabase = await this.cloudManagementService
+                    .ProvisionSqlDatabaseAsync(
+                        projectName,
+                        environmentName,
+                        sqlServer);
+
+                IWebApp webApp = await this.cloudManagementService
+                    .ProvisionWebAppAsync(
+                        projectName,
+                        environmentName,
+                        sqlDatabase.ConnectionString,
+                        resourceGroup,
+                        appServicePlan);
+            }
+        }
+
+        private static List<string> RetrieveEnvironments(CloudAction cloudAction) =>
+            cloudAction?.Environments ?? new List<string>();
+    }
+}
---
+++ Taarafo.Core.Infrastructure.Provision/Services/Processings/ICloudManagementProcessingService.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace Taarafo.Core.Infrastructure.Provision.Services.Processings
+{
+    public interface ICloudManagementProcessingService
+    {
+        ValueTask ProcessAsync();
+    }
+}
