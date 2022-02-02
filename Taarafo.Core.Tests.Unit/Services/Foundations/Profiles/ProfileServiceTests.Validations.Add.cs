f727987a31770fa4ab34574b6356a1a70702185d
Ricky <rcruzfreelance@outlook.com>
2022-02-02 19:21:43 -0400

    ShouldThrowValidationExceptionOnAddIfProfileIsInvalidAndLogItAsync -> FAIL

--- Taarafo.Core.Tests.Unit/Services/Foundations/Profiles/ProfileServiceTests.Validations.Add.cs
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Profiles/ProfileServiceTests.Validations.Add.cs
@@ -45,5 +45,69 @@ namespace Taarafo.Core.Tests.Unit.Services.Foundations.Profiles
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfProfileIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidProfile = new Profile
+            {
+                Username = invalidText,
+                Email = invalidText
+            };
+
+            var invalidProfileException =
+                new InvalidProfileException();
+
+            invalidProfileException.AddData(
+                key: nameof(Profile.Id),
+                values: "Id is required");
+
+            invalidProfileException.AddData(
+                key: nameof(Profile.Name),
+                values: "Text is required"); 
+            
+            invalidProfileException.AddData(
+                key: nameof(Profile.Username),
+                values: "Text is required");
+            
+            invalidProfileException.AddData(
+                key: nameof(Profile.Email),
+                values: "Text is required");
+            
+            invalidProfileException.AddData(
+                key: nameof(Profile.CreatedDate),
+                values: "Date is required");
+            
+            invalidProfileException.AddData(
+                key: nameof(Profile.UpdatedDate),
+                values: "Date is required");
+
+            var expectedProfileValidationException =
+                new ProfileValidationException(invalidProfileException);
+
+            // when
+            ValueTask<Profile> addProfileTask =
+                this.profileService.AddProfileAsync(invalidProfile);
+
+            // then
+            await Assert.ThrowsAsync<ProfileValidationException>(() =>
+                addProfileTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedProfileValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertProfileAsync(invalidProfile),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
