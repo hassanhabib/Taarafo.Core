30f5b3728a969656e367bf3907c5538e8fda5008
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-02-23 19:20:25 -0800

    BROKERS : Inserted group

--- Taarafo.Core/Brokers/Storages/StorageBroker.Groups.cs
+++ Taarafo.Core/Brokers/Storages/StorageBroker.Groups.cs
@@ -4,12 +4,24 @@
 // ---------------------------------------------------------------
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Taarafo.Core.Models.Groups;
 
 namespace Taarafo.Core.Brokers.Storages
 {
-	public partial class StorageBroker
-	{
-		public DbSet<Group> Groups { get; set; }	
-	}
+    public partial class StorageBroker
+    {
+        public DbSet<Group> Groups { get; set; }
+        public async ValueTask<Group> InsertProfileAsync(Group group)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<Group> groupEntityEntry =
+                await broker.Groups.AddAsync(group);
+
+            await broker.SaveChangesAsync();
+
+            return groupEntityEntry.Entity;
+        }
+    }
 }
