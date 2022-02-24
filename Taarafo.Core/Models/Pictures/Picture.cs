bd878b343642f672f056448349a2ec12857c9a94
evangelinedrink <Evangelinedrink@gmail.com>
2022-02-23 21:01:24 -0800

    DATA: Created Only Pictures Model

---
+++ Taarafo.Core/Models/Pictures/Picture.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Taarafo.Core.Models.Posts;
+
+namespace Taarafo.Core.Models.Pictures
+{
+    public class Picture
+    {
+		public Guid Id { get; set; }
+		public string Description { get; set; }
+		public DateTime UploadedDate { get; set; }
+
+		public Guid PostId { get; set; }
+		public Post Post { get; set; }
+	}
+}
