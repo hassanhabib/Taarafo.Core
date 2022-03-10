33cb37219c7371063947353b484f11c4b08f69d2
Bbereket1 <berekettsegaye13@gmail.com>
2022-03-09 16:07:38 -0800

    BROKERS: Update Group

--- Taarafo.Core/Brokers/Storages/StorageBroker.Groups.cs
+++ Taarafo.Core/Brokers/Storages/StorageBroker.Groups.cs
@@ -44,5 +44,19 @@ namespace Taarafo.Core.Brokers.Storages
 
             return await broker.Groups.FindAsync(groupId);
         }
+
+        public async ValueTask<Group> UpdateGroupAsync(Group group)
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            EntityEntry<Group> groupEntityEntry =
+                broker.Groups.Update(group);
+
+            await broker.SaveChangesAsync();
+
+            return groupEntityEntry.Entity;
+        }
+
     }
 }
