ea3c9d87a4556d5ab803ea6e96a877b0c1c4d9fb
tsatay <tedh1677@gmail.com>
2022-03-12 21:04:56 -0500

    BROKERS: Insert Post Impression

--- Taarafo.Core/Brokers/Storages/StorageBroker.PostImpressions.cs
+++ Taarafo.Core/Brokers/Storages/StorageBroker.PostImpressions.cs
@@ -3,7 +3,9 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Taarafo.Core.Models.PostImpressions;
 
 namespace Taarafo.Core.Brokers.Storages
@@ -11,5 +13,17 @@ namespace Taarafo.Core.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<PostImpression> PostImpressions { get; set; }
+
+        public async ValueTask<PostImpression> InsertPostImpressionAsync(PostImpression postImpression)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<PostImpression> postImpressionEntityEntry =
+                await broker.PostImpressions.AddAsync(postImpression);
+
+            await broker.SaveChangesAsync();
+            return postImpressionEntityEntry.Entity;      
+        }
+
     }
 }
