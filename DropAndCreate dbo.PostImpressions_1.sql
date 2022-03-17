2fbfc92356d985e12b5dcd6fb49f7fbc27d1c1b4
tsatay <tedh1677@gmail.com>
2022-03-16 23:28:09 -0400

    DATA: Change Migration

---
+++ DropAndCreate dbo.PostImpressions_1.sql
@@ -0,0 +1,23 @@
+﻿USE [TaarafoCoreDb]
+GO
+
+/****** Object: Table [dbo].[PostImpressions] Script Date: 3/16/2022 11:03:22 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE [dbo].[PostImpressions];
+
+
+GO
+CREATE TABLE [dbo].[PostImpressions] (
+    [PostId]      UNIQUEIDENTIFIER   NOT NULL,
+    [ProfileId]   UNIQUEIDENTIFIER   NOT NULL,
+    [CurrentDate] DATETIMEOFFSET (7) NOT NULL,
+    [UpdateDate]  DATETIMEOFFSET (7) NOT NULL,
+    [Impression]  INT                NOT NULL
+);
+
+
