7210efd837cf550e9c495a212dc902ec03d5ad76
tsatay <tedh1677@gmail.com>
2022-04-04 20:15:23 -0400

    DATA: Create Post Impression Method and Broker

---
+++ Taarafo.Core/Models/PostImpressions/PostImpression.cs
@@ -0,0 +1,24 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Taarafo.Core.Models.Posts;
+using Taarafo.Core.Models.Profiles;
+
+namespace Taarafo.Core.Models.PostImpressions
+{
+    public class PostImpression
+    {
+        public Guid PostId { get; set; }
+        public Post Post { get; set; }
+
+        public Guid ProfileId { get; set; }
+        public Profile Profile { get; set; }
+
+        public ImpressionType Impressions { get; set; }
+        public DateTimeOffset  CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
