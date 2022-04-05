817ad8e5385a3cc5b2fcea83450367578f2d6eb8
tsatay <tedh1677@gmail.com>
2022-04-04 21:33:35 -0400

    DATA: Create Post Impressions References and Migration

---
+++ Taarafo.Core/Brokers/Storages/StorageBroker.PostImpressions.References.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using Taarafo.Core.Models.PostImpressions;
+
+namespace Taarafo.Core.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public static void AddPostImpressionReferences(ModelBuilder modelBuilders)
+        {
+            modelBuilders.Entity<PostImpression>()
+                .HasKey(postImpression => new { postImpression.PostId, postImpression.ProfileId });
+
+            modelBuilders.Entity<PostImpression>()
+                .HasOne(postImpression => postImpression.Post)
+                .WithMany(post => post.PostImpressions)
+                .HasForeignKey(postImpression => postImpression.PostId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilders.Entity<PostImpression>()
+                .HasOne(postImpression => postImpression.Profile)
+                .WithMany(profile => profile.PostImpressions)
+                .HasForeignKey(postImpression => postImpression.ProfileId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
