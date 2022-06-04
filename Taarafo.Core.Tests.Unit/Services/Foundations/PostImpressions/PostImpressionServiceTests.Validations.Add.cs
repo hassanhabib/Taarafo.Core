109444ab9719c2aa1a86f56fedd67d1951c4fbdc
evangelinedrink <Evangelinedrink@gmail.com>
2022-06-03 23:43:39 -0700

    ShouldThrowValidationExceptionOnAddIfPostImpressionIsInvalidAndLogItAsync -> FAIL

--- Taarafo.Core.Tests.Unit/Services/Foundations/PostImpressions/PostImpressionServiceTests.Validations.Add.cs
+++ Taarafo.Core.Tests.Unit/Services/Foundations/PostImpressions/PostImpressionServiceTests.Validations.Add.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using Moq;
 using Taarafo.Core.Models.PostImpressions;
 using Taarafo.Core.Models.PostImpressions.Exceptions;
+using Taarafo.Core.Models.Posts;
 using Xunit;
 
 namespace Taarafo.Core.Tests.Unit.Services.Foundations.PostImpressions
@@ -41,6 +42,69 @@ namespace Taarafo.Core.Tests.Unit.Services.Foundations.PostImpressions
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedPostImpressionValidationException))),
                         Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfPostImpressionIsInvalidAndLogItAsync()
+        {
+            //given
+            Guid invalidGuid = Guid.Empty;
+
+            var invalidPostImpression = new PostImpression
+            {
+                PostId = invalidGuid,
+                ProfileId = invalidGuid
+            };
+
+            var invalidPostImpressionException =
+                new InvalidPostImpressionException();
+
+            invalidPostImpressionException.AddData(
+                key: nameof(PostImpression.PostId),
+                values: "Post Id is required.");
+
+            invalidPostImpressionException.AddData(
+                key: nameof(PostImpression.Post),
+                values: "Post is required.");
+
+            invalidPostImpressionException.AddData(
+                key: nameof(PostImpression.ProfileId),
+                values: "Profile Id is required.");
+
+            invalidPostImpressionException.AddData(
+                key: nameof(PostImpression.Profile),
+                values: "Profile is required.");
+
+            invalidPostImpressionException.AddData(
+                key: nameof(PostImpression.CreatedDate),
+                values: "Date is required.");
+
+            invalidPostImpressionException.AddData(
+                key: nameof(PostImpression.UpdatedDate),
+                values: "Date is required.");
+
+            var expectedPostImpressionValidationException =
+                new PostImpressionValidationException(invalidPostImpressionException);
+
+            //when
+            ValueTask<PostImpression> addPostImpressionTask =
+                this.postImpressionService.AddPostImpressions(invalidPostImpression);
+
+            //then
+            await Assert.ThrowsAsync<PostImpressionValidationException>(() =>
+                addPostImpressionTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostImpressionValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertPostImpressionAsync(invalidPostImpression),
+                    Times.Never);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
