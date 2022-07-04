17b250730dc086862c1e223bdf2caae5bae23b43
evangelinedrink <Evangelinedrink@gmail.com>
2022-07-03 19:28:32 -0700

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

--- Taarafo.Core/Services/Foundations/PostImpressions/PostImpressionService.cs
+++ Taarafo.Core/Services/Foundations/PostImpressions/PostImpressionService.cs
@@ -3,7 +3,6 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Taarafo.Core.Brokers.DateTimes;
