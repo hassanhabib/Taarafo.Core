52018d341d7383f757da47e404535acf165eafa3
Hassan Habib <hassanhabib@live.com>
2022-07-04 11:35:02 -0700

    INFRA: Provision Scripts

--- Taarafo.Core.Infrastructure.Build/Program.cs
+++ Taarafo.Core.Infrastructure.Build/Program.cs
@@ -3,71 +3,8 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
-using ADotNet.Clients;
-using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
-using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
-using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+using Taarafo.Core.Infrastructure.Build.Services;
 
-var adotNetClient = new ADotNetClient();
-
-var githubPipeline = new GithubPipeline
-{
-    Name = ".Net",
-
-    OnEvents = new Events
-    {
-        Push = new PushEvent
-        {
-            Branches = new string[] { "master" }
-        },
-
-        PullRequest = new PullRequestEvent
-        {
-            Branches = new string[] { "master" }
-        }
-    },
-
-    Jobs = new Jobs
-    {
-        Build = new BuildJob
-        {
-            RunsOn = BuildMachines.Windows2019,
-
-            Steps = new List<GithubTask>
-            {
-                new CheckoutTaskV2
-                {
-                    Name = "Check Out"
-                },
-
-                new SetupDotNetTaskV1
-                {
-                    Name = "Setup Dot Net Version",
-
-                    TargetDotNetVersion = new TargetDotNetVersion
-                    {
-                        DotNetVersion = "7.0.100-preview.1.22110.4",
-                        IncludePrerelease = true
-                    }
-                },
-
-                new RestoreTask
-                {
-                    Name = "Restore"
-                },
-
-                new DotNetBuildTask
-                {
-                    Name = "Build"
-                },
-
-                new TestTask
-                {
-                    Name = "Test"
-                }
-            }
-        }
-    }
-};
-
-adotNetClient.SerializeAndWriteToFile(githubPipeline, "../../../../.github/workflows/dotnet.yml");
+var integrationService = new IntegrationService();
+integrationService.GenerateBuildScripts();
+integrationService.GenerateProvisionScripts();
