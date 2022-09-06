fe3cd10002c8a550f073cf2039417a19efa74744
blouflashdb <45914736+blouflashdb@users.noreply.github.com>
2022-09-06 20:33:10 +0200

    Implement delete group post

--- Taarafo.Core/Brokers/Storages/StorageBroker.GroupPosts.cs
+++ Taarafo.Core/Brokers/Storages/StorageBroker.GroupPosts.cs
@@ -3,7 +3,9 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Taarafo.Core.Models.GroupPosts;
 
 namespace Taarafo.Core.Brokers.Storages
@@ -11,5 +13,18 @@ namespace Taarafo.Core.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<GroupPost> GroupPosts { get; set; }
+
+        public async ValueTask<GroupPost> DeleteGroupPostAsync(GroupPost groupPost)
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            EntityEntry<GroupPost> groupPostEntityEntry =
+                broker.GroupPosts.Remove(groupPost);
+
+            await broker.SaveChangesAsync();
+
+            return groupPostEntityEntry.Entity;
+        }
     }
 }
