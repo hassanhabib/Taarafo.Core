003e1c9c8df6def9ffa38a3ae5eef8a451a5e96b
hassaero110 <h_raza110@outlook.com>
2022-09-08 14:04:55 +0100

    BROKERS: Select All Events

--- Taarafo.Core/Brokers/Storages/IStorageBroker.Events.cs
+++ Taarafo.Core/Brokers/Storages/IStorageBroker.Events.cs
@@ -1,10 +1,13 @@
 ﻿using Taarafo.Core.Models.Events;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Taarafo.Core.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
         ValueTask<Event> InsertEventAsync(Event @event);
+
+        IQueryable<Event> SelectAllEvents();
     }
 }
