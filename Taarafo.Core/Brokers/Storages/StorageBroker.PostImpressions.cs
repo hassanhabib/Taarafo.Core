cac178f6676decd7d89617d347cf06ffad2df41d
evangelinedrink <Evangelinedrink@gmail.com>
2022-09-18 02:26:54 +0200

    BROKERS: Select Post Impression By Ids

--- Taarafo.Core/Brokers/Storages/StorageBroker.PostImpressions.cs
+++ Taarafo.Core/Brokers/Storages/StorageBroker.PostImpressions.cs
@@ -3,6 +3,7 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -17,7 +18,7 @@ namespace Taarafo.Core.Brokers.Storages
 
         public async ValueTask<PostImpression> InsertPostImpressionAsync(PostImpression postImpression)
         {
-            using var broker =
+            var broker =
                 new StorageBroker(this.configuration);
 
             EntityEntry<PostImpression> postImpressionEntityEntry =
@@ -34,6 +35,14 @@ namespace Taarafo.Core.Brokers.Storages
                 new StorageBroker(this.configuration);
 
             return broker.PostImpressions;
+        }
+
+        public async ValueTask<PostImpression> SelectPostImpressionByIdsAsync(Guid PostId, Guid ProfileId)
+        {
+            var broker =
+                new StorageBroker(this.configuration);
+
+            return await broker.PostImpressions.FindAsync(PostId, ProfileId);
         }
 
         public async ValueTask<PostImpression> UpdatePostImpressionAsync(PostImpression postImpression)
