93f0151f9a0c5933dab24030b15aba6e5419451d
Hassan Raza <hassan.raza3@nhs.net>
2022-10-12 10:50:20 +0100

    Removing Line Space

--- Taarafo.Core/Brokers/Storages/IStorageBroker.Events.cs
+++ Taarafo.Core/Brokers/Storages/IStorageBroker.Events.cs
@@ -10,7 +10,6 @@ namespace Taarafo.Core.Brokers.Storages
         ValueTask<Event> InsertEventAsync(Event @event);
         IQueryable<Event> SelectAllEvents();
         ValueTask<Event> SelectEventByIdAsync(Guid eventId);
-
         ValueTask<Event> UpdateEventAsync(Event @event);
     }
 }
