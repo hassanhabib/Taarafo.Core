3cc4d4ba6dbd83c89dcc70931b949f66dd6c12d7
Yeasin Arafat <yeasinarafat10@gmail.com>
2022-10-20 10:14:19 +0600

    ShouldAddGroupPostAsync -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/GroupPosts/GroupPostServiceTests.cs
@@ -0,0 +1,51 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Moq;
+using Taarafo.Core.Brokers.DateTimes;
+using Taarafo.Core.Brokers.Loggings;
+using Taarafo.Core.Brokers.Storages;
+using Taarafo.Core.Models.GroupPosts;
+using Taarafo.Core.Services.Foundations.GroupPosts;
+using Taarafo.Core.Services.Foundations.Groups;
+using Tynamix.ObjectFiller;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.GroupPosts
+{
+    public partial class GroupPostServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly IGroupPostService groupPostService;
+
+        public GroupPostServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.groupPostService = new GroupPostService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static GroupPost CreateRandomGroupPost(DateTimeOffset dates) =>
+            CreateGroupPostFiller(dates).Create();
+
+        private static Filler<GroupPost> CreateGroupPostFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<GroupPost>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
---
+++ Taarafo.Core/Services/Foundations/GroupPosts/GroupPostService.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Taarafo.Core.Brokers.DateTimes;
+using Taarafo.Core.Brokers.Loggings;
+using Taarafo.Core.Brokers.Storages;
+using Taarafo.Core.Models.GroupPosts;
+
+namespace Taarafo.Core.Services.Foundations.GroupPosts
+{
+    public partial class GroupPostService : IGroupPostService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public GroupPostService(IStorageBroker storageBroker, 
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<GroupPost> AddGroupPostAsync(GroupPost groupPost)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
