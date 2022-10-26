2b06cbfd3e85710eb1f1a30302eae1a8cb48f33a
Yeasin Arafat <yeasinarafat10@gmail.com>
2022-10-26 19:32:45 +0600

    ShouldThrowValidationExceptionOnAddIfGroupPostIsInvalidAndLogItAsync -> FAIL

--- Taarafo.Core.Tests.Unit/Services/Foundations/GroupPosts/GroupPostServiceTests.Validations.Add.cs
+++ Taarafo.Core.Tests.Unit/Services/Foundations/GroupPosts/GroupPostServiceTests.Validations.Add.cs
@@ -3,11 +3,13 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
 using Taarafo.Core.Models.GroupPosts;
 using Taarafo.Core.Models.GroupPosts.Exceptions;
+using Taarafo.Core.Models.PostImpressions.Exceptions;
 using Xunit;
 
 namespace Taarafo.Core.Tests.Unit.Services.Foundations.GroupPosts
@@ -42,6 +44,65 @@ namespace Taarafo.Core.Tests.Unit.Services.Foundations.GroupPosts
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedGroupPostValidationException))),
                     Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfGroupPostIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidGuid = Guid.Empty;
+
+            var invalidGroupPost = new GroupPost
+            {
+                GroupId = invalidGuid,
+                PostId = invalidGuid
+            };
+
+            var invalidGrouPostException = 
+                new InvalidGroupPostException();
+
+            invalidGrouPostException.AddData(
+                key: nameof(GroupPost.GroupId),
+                values: "Id is required");
+
+            invalidGrouPostException.AddData(
+                key: nameof(GroupPost.Group),
+                values: "Object is required");
+
+            invalidGrouPostException.AddData(
+                key: nameof(GroupPost.PostId),
+                values: "Id is required");
+
+            invalidGrouPostException.AddData(
+                key: nameof(GroupPost.Post),
+                values: "Object is required");
+            
+            var expectedGroupPostValidationException =
+                new GroupPostValidationException(invalidGrouPostException);
+
+            // when
+            ValueTask<GroupPost> addGroupPostTask =
+                this.groupPostService.AddGroupPostAsync(invalidGroupPost);
+
+            GroupPostValidationException actualGroupPostValidationException =
+                await Assert.ThrowsAsync<GroupPostValidationException>(
+                    addGroupPostTask.AsTask);
+
+            // then
+            actualGroupPostValidationException.Should().BeEquivalentTo(
+                expectedGroupPostValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGroupPostValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertGroupPostAsync(invalidGroupPost),
+                    Times.Never);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
--- Taarafo.Core/Services/Foundations/GroupPosts/GroupPostService.Validations.cs
+++ Taarafo.Core/Services/Foundations/GroupPosts/GroupPostService.Validations.cs
@@ -3,8 +3,10 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
 using Taarafo.Core.Models.GroupPosts;
 using Taarafo.Core.Models.GroupPosts.Exceptions;
+using Taarafo.Core.Models.PostImpressions;
 
 namespace Taarafo.Core.Services.Foundations.GroupPosts
 {
@@ -13,7 +15,25 @@ namespace Taarafo.Core.Services.Foundations.GroupPosts
         public static void ValidateGroupPostOnAdd(GroupPost groupPost)
         {
             ValidateGroupPostIsNotNull(groupPost);
+
+            Validate(
+                (Rule: IsInvalid(groupPost.GroupId), Parameter: nameof(GroupPost.GroupId)), 
+                (Rule: IsInvalid(groupPost.Group), Parameter: nameof(GroupPost.Group)),
+                (Rule: IsInvalid(groupPost.PostId), Parameter: nameof(GroupPost.PostId)),
+                (Rule: IsInvalid(groupPost.Post), Parameter: nameof(GroupPost.Post)));
         }
+
+        private static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == Guid.Empty,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(object @object) => new
+        {
+            Condition = @object is null,
+            Message = "Object is required"
+        };
 
         private static void ValidateGroupPostIsNotNull(GroupPost groupPost)
         {
@@ -21,6 +41,23 @@ namespace Taarafo.Core.Services.Foundations.GroupPosts
             {
                 throw new NullGroupPostException();
             }
+        }
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidGroupPostException = new InvalidGroupPostException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidGroupPostException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidGroupPostException.ThrowIfContainsErrors();
         }
     }
 }
