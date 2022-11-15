74237ab76a09d6e871c0dca3ac8a5d4c3ad60b6c
Christo du Toit <christo@dutoit.co.uk>
2022-11-15 18:48:34 +0000

    CODE RUB: Enabled ODATA Support

--- Taarafo.Core/Startup.cs
+++ Taarafo.Core/Startup.cs
@@ -9,10 +9,14 @@ using Microsoft.AspNetCore.OData;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
 using Microsoft.OpenApi.Models;
 using Taarafo.Core.Brokers.DateTimes;
 using Taarafo.Core.Brokers.Loggings;
 using Taarafo.Core.Brokers.Storages;
+using Taarafo.Core.Models.Comments;
+using Taarafo.Core.Models.Posts;
 using Taarafo.Core.Services.Foundations.Comments;
 using Taarafo.Core.Services.Foundations.Groups;
 using Taarafo.Core.Services.Foundations.Posts;
@@ -20,72 +24,95 @@ using Taarafo.Core.Services.Foundations.Profiles;
 
 namespace Taarafo.Core
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration) =>
-			Configuration = configuration;
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLogging();
 
             services.AddControllers()
-                .AddOData(options => options.Select().Filter().Expand().OrderBy().Count().SetMaxTop(25));
+                .AddOData(options =>
+                {
+                    options.AddRouteComponents("odata", GetEdmModel());
+
+                    options
+                        .Select()
+                        .Filter()
+                        .Expand()
+                        .OrderBy()
+                        .Count()
+                        .SetMaxTop(25);
+                });
 
             services.AddDbContext<StorageBroker>();
             AddBrokers(services);
             AddServices(services);
 
-			services.AddSwaggerGen(options =>
-			{
-				var openApiInfo = new OpenApiInfo
-				{
-					Title = "Taarafo.Core",
-					Version = "v1"
-				};
+            services.AddSwaggerGen(options =>
+            {
+                var openApiInfo = new OpenApiInfo
+                {
+                    Title = "Taarafo.Core",
+                    Version = "v1"
+                };
 
-				options.SwaggerDoc(
-					name: "v1",
-					info: openApiInfo);
-			});
-		}
+                options.SwaggerDoc(
+                    name: "v1",
+                    info: openApiInfo);
+            });
+        }
 
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-				app.UseSwagger();
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
 
-				app.UseSwaggerUI(options =>
-				{
-					options.SwaggerEndpoint(
-						url: "/swagger/v1/swagger.json",
-						name: "Taarafo.Core v1");
-				});
-			}
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint(
+                        url: "/swagger/v1/swagger.json",
+                        name: "Taarafo.Core v1");
+                });
+            }
 
-			app.UseHttpsRedirection();
-			app.UseRouting();
-			app.UseAuthorization();
-			app.UseEndpoints(endpoints => endpoints.MapControllers());
-		}
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
 
-		private static void AddServices(IServiceCollection services)
-		{
-			services.AddTransient<ICommentService, CommentService>();
-			services.AddTransient<IPostService, PostService>();
-			services.AddTransient<IProfileService, ProfileService>();
-			services.AddTransient<IGroupService, GroupService>();
-		}
+        private static void AddServices(IServiceCollection services)
+        {
+            services.AddTransient<ICommentService, CommentService>();
+            services.AddTransient<IPostService, PostService>();
+            services.AddTransient<IProfileService, ProfileService>();
+            services.AddTransient<IGroupService, GroupService>();
+        }
 
-		private static void AddBrokers(IServiceCollection services)
-		{
-			services.AddTransient<IStorageBroker, StorageBroker>();
-			services.AddTransient<ILoggingBroker, LoggingBroker>();
-			services.AddTransient<IDateTimeBroker, DateTimeBroker>();
-		}
-	}
+        private static void AddBrokers(IServiceCollection services)
+        {
+            services.AddTransient<IStorageBroker, StorageBroker>();
+            services.AddTransient<ILoggingBroker, LoggingBroker>();
+            services.AddTransient<IDateTimeBroker, DateTimeBroker>();
+        }
+
+        private IEdmModel GetEdmModel()
+        {
+            ODataConventionModelBuilder builder =
+                new ODataConventionModelBuilder();
+
+            builder.EntitySet<Comment>("Comments");
+            builder.EntitySet<Post>("Posts");
+            builder.EnableLowerCamelCase();
+
+            return builder.GetEdmModel();
+        }
+    }
 }
