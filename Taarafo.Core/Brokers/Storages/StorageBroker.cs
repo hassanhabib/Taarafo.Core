656175093c602d576c1131785f2f413efadd1553
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-11-25 21:30:45 +0300

    CODE RUB: Upgrade InsertProfileAsync

--- Taarafo.Core/Brokers/Storages/StorageBroker.cs
+++ Taarafo.Core/Brokers/Storages/StorageBroker.cs
@@ -5,33 +5,31 @@
 
 using System.Threading.Tasks;
 using EFxceptions;
-using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace Taarafo.Core.Brokers.Storages
 {
-	public partial class StorageBroker : EFxceptionsContext, IStorageBroker
-	{
-		private readonly IConfiguration configuration;
+    public partial class StorageBroker : EFxceptionsContext, IStorageBroker
+    {
+        private readonly IConfiguration configuration;
 
-		public StorageBroker(IConfiguration configuration)
-		{
-			this.configuration = configuration;
-			this.Database.Migrate();
-		}
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
 
-		private async ValueTask<T> InsertAsync<T>(T @object)
-		{
-			var broker = new StorageBroker(this.configuration);
-			broker.Entry(@object).State = EntityState.Added;
-			await broker.SaveChangesAsync();
+        private async ValueTask<T> InsertAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Added;
+            await this.SaveChangesAsync();
 
-			return @object;
-		}
+            return @object;
+        }
 
-		private async ValueTask<T> SelectAsync<T>(params object[] objectIds) where T : class =>
-			await FindAsync<T>(objectIds);
+        private async ValueTask<T> SelectAsync<T>(params object[] objectIds) where T : class =>
+            await FindAsync<T>(objectIds);
 
         private async ValueTask<T> UpdateAsync<T>(T @object)
         {
@@ -43,20 +41,20 @@ namespace Taarafo.Core.Brokers.Storages
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			AddCommentConfigurations(modelBuilder);
-			AddGroupPostConfigurations(modelBuilder);
-			AddPostImpressionConfigurations(modelBuilder);
-		}
-       
+        {
+            AddCommentConfigurations(modelBuilder);
+            AddGroupPostConfigurations(modelBuilder);
+            AddPostImpressionConfigurations(modelBuilder);
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		{
-			string connectionString = this.configuration
-				.GetConnectionString(name: "DefaultConnection");
+        {
+            string connectionString = this.configuration
+                .GetConnectionString(name: "DefaultConnection");
 
-			optionsBuilder.UseSqlServer(connectionString);
-		}
+            optionsBuilder.UseSqlServer(connectionString);
+        }
 
-		public override void Dispose() { }
-	}
+        public override void Dispose() { }
+    }
 }
