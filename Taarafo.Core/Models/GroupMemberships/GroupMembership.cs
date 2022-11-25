218445114bc91816a759314a6a2c864d01a77d61
Khusniddin <isroilovkhusni@gmail.com>
2022-11-26 03:34:11 +0500

    CODE RUB: Formatting

--- Taarafo.Core/Models/GroupMemberships/GroupMembership.cs
+++ Taarafo.Core/Models/GroupMemberships/GroupMembership.cs
@@ -13,12 +13,11 @@ namespace Taarafo.Core.Models.GroupMemberships
     {
         public Guid Id { get; set; }
         public Guid ProfileId { get; set; }
-        public Profile Profile { get; set; }
-
         public Guid GroupId { get; set; }
-        public Group Group { get; set; }
-
         public GroupMembershipStatus Status { get; set; }
         public DateTimeOffset MembershipDate { get; set; }
+
+        public Group Group { get; set; }
+        public Profile Profile { get; set; }
     }
 }
