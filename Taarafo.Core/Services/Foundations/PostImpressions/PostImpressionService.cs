4884e41a6f9ffb859b000850d8138b86acb7de6e
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-12-03 08:58:29 +0300

    ShouldReturnPostImpressions ->PASS

--- Taarafo.Core/Services/Foundations/PostImpressions/PostImpressionService.cs
+++ Taarafo.Core/Services/Foundations/PostImpressions/PostImpressionService.cs
@@ -37,6 +37,6 @@ namespace Taarafo.Core.Services.Foundations.PostImpressions
         });
 
         public IQueryable<PostImpression> RetrieveAllPostImpressions() =>
-            throw new System.NotImplementedException();
+            this.storageBroker.SelectAllPostImpressions();
     }
 }
