f30fd55ffff93aa2f4b50ae7c524cbdd97046005
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-12-03 08:52:37 +0300

    ShouldReturnPostImpressions ->FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/PostImpressions/PostImpressionServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,42 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using Taarafo.Core.Models.PostImpressions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.PostImpressions
+{
+    public partial class PostImpressionServiceTests
+    {
+        [Fact]
+        public void ShouldReturnPostImpressions()
+        {
+            //given
+            IQueryable<PostImpression> randomPostImpressions = CreateRandomPostImpressions();
+            IQueryable<PostImpression> storagePostImpressions = randomPostImpressions;
+            IQueryable<PostImpression> expectedPostImpressions = storagePostImpressions;
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.SelectAllPostImpressions())
+                .Returns(storagePostImpressions);
+
+            //when
+            IQueryable<PostImpression> actualPostImpressions =
+                this.postImpressionService.RetrieveAllPostImpressions();
+
+            //then
+            actualPostImpressions.Should().BeEquivalentTo(expectedPostImpressions);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllPostImpressions(), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- Taarafo.Core/Services/Foundations/PostImpressions/PostImpressionService.cs
+++ Taarafo.Core/Services/Foundations/PostImpressions/PostImpressionService.cs
@@ -3,6 +3,7 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System.Linq;
 using System.Threading.Tasks;
 using Taarafo.Core.Brokers.DateTimes;
 using Taarafo.Core.Brokers.Loggings;
@@ -11,28 +12,31 @@ using Taarafo.Core.Models.PostImpressions;
 
 namespace Taarafo.Core.Services.Foundations.PostImpressions
 {
-	public partial class PostImpressionService : IPostImpressionService
-	{
-		private readonly IStorageBroker storageBroker;
-		private readonly IDateTimeBroker dateTimeBroker;
-		private readonly ILoggingBroker loggingBroker;
+    public partial class PostImpressionService : IPostImpressionService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
 
-		public PostImpressionService(
-			IStorageBroker storageBroker,
-			IDateTimeBroker dateTimeBroker,
-			ILoggingBroker loggingBroker)
-		{
-			this.storageBroker = storageBroker;
-			this.dateTimeBroker = dateTimeBroker;
-			this.loggingBroker = loggingBroker;
-		}
+        public PostImpressionService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
 
-		public ValueTask<PostImpression> AddPostImpressions(PostImpression postImpression) =>
-		TryCatch(async () =>
-		{
-			ValidatePostImpressionOnAdd(postImpression);
+        public ValueTask<PostImpression> AddPostImpressions(PostImpression postImpression) =>
+        TryCatch(async () =>
+        {
+            ValidatePostImpressionOnAdd(postImpression);
 
-			return await this.storageBroker.InsertPostImpressionAsync(postImpression);
-		});
-	}
+            return await this.storageBroker.InsertPostImpressionAsync(postImpression);
+        });
+
+        public IQueryable<PostImpression> RetrieveAllPostImpressions() =>
+            throw new System.NotImplementedException();
+    }
 }
