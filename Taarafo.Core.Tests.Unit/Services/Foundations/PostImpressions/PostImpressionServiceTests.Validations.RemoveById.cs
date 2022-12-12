f0a5bb4d208a352a4db64c09f091ee517b4c7683
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-12-12 12:29:14 +0300

    ShouldThrowValidatonExceptionOnRemoveWhenProfileIdIsInvalidAndLogItAsync ->FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/PostImpressions/PostImpressionServiceTests.Validations.RemoveById.cs
@@ -0,0 +1,62 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Taarafo.Core.Models.PostImpressions;
+using Taarafo.Core.Models.PostImpressions.Exceptions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.PostImpressions
+{
+    public partial class PostImpressionServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync()
+        {
+            //given
+            Guid randomPostId= Guid.NewGuid();
+            Guid randomProfileId= default;
+            Guid inputPostId = randomPostId;
+            Guid inputProfileId= randomProfileId;
+
+            var invalidPostImpressionException = new InvalidPostImpressionException(
+                    parameterName:nameof(PostImpression.PostId),
+                    parameterValue:inputPostId);
+
+            var expectedPostImpressionValidationException =
+                new PostImpressionValidationException(invalidPostImpressionException);
+
+            //when
+            ValueTask<PostImpression> removePostImpressionTask= 
+                    this.postImpressionService.RemovePostImpressionByIdAsync(inputPostId, inputProfileId);
+
+            //then
+            await Assert.ThrowsAsync<PostImpressionValidationException>(()=>
+                removePostImpressionTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker=>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostImpressionValidationException))),Times.Once);
+
+            this.storageBrokerMock.Verify(broker=>
+                broker.SelectPostImpressionByIdsAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<Guid>()),
+                        Times.Never);
+
+            this.storageBrokerMock.Verify(broker=>
+                broker.DeletePostImpressionAsync(
+                    It.IsAny<PostImpression>()),Times.Never);
+            
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- Taarafo.Core/Models/PostImpressions/Exceptions/InvalidPostImpressionException.cs
+++ Taarafo.Core/Models/PostImpressions/Exceptions/InvalidPostImpressionException.cs
@@ -9,7 +9,13 @@ namespace Taarafo.Core.Models.PostImpressions.Exceptions
 {
 	public class InvalidPostImpressionException : Xeption
 	{
-		public InvalidPostImpressionException()
+        public InvalidPostImpressionException(string parameterName, object parameterValue)
+            : base(message: $"Invalid postImpression, " +
+                 $"parameter name: {parameterName}, " +
+                 $"parameter value: {parameterValue}.")
+        { }
+
+        public InvalidPostImpressionException()
 			: base(message: "Invalid post impression. Please correct the errors and try again.")
 		{ }
 	}
