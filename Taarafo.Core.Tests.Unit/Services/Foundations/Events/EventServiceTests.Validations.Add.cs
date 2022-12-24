fd22e4e794a06fb5db1404048e70b7a3f0d9c23b
Khusniddin <isroilovkhusni@gmail.com>
2022-12-25 01:34:16 +0500

    ShouldThrowValidationExceptionOnAddIfEventIsInvalidAndLogItAsync -> FAIL

--- Taarafo.Core.Tests.Unit/Services/Foundations/Events/EventServiceTests.Validations.Add.cs
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Events/EventServiceTests.Validations.Add.cs
@@ -44,5 +44,82 @@ namespace Taarafo.Core.Tests.Unit.Services.Foundations.Events
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfEventIsInvalidAndLogItAsync(
+            string invalidString)
+        {
+            //given
+            Event invalidEvent = new Event
+            {
+                Name = invalidString,
+            };
+
+            var invalidEventException = new InvalidEventException();
+
+            invalidEventException.AddData(
+                key: nameof(Event.Id),
+                values: "Id is required"); 
+            
+            invalidEventException.AddData(
+                key: nameof(Event.Name),
+                values: "Text is required");
+
+            invalidEventException.AddData(
+              key: nameof(Event.Date),
+              values: "Date is required");
+
+            invalidEventException.AddData(
+              key: nameof(Event.Location),
+              values: "Location is required");
+
+            invalidEventException.AddData(
+                key: nameof(Event.Image),
+                values: "Text is required");
+
+            invalidEventException.AddData(
+              key: nameof(Event.CreatedBy),
+              values: "Id is required");
+
+            invalidEventException.AddData(
+              key: nameof(Event.CreatedDate),
+              values: "Date is required");
+
+            invalidEventException.AddData(
+              key: nameof(Event.UpdatedBy),
+              values: "Id is required");
+
+            invalidEventException.AddData(
+              key: nameof(Event.UpdatedDate),
+              values: "Date is required");
+
+            EventValidationException expectedEventValidationException =
+                new EventValidationException(invalidEventException);
+
+            //when
+            ValueTask<Event> addEventTask = 
+                this.eventService.AddEventAsync(invalidEvent);
+
+            EventValidationException actualEventValidationException =
+                await Assert.ThrowsAsync<EventValidationException>(addEventTask.AsTask);
+
+            //then
+            actualEventValidationException.Should().BeEquivalentTo(
+                expectedEventValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedEventValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEventAsync(It.IsAny<Event>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
