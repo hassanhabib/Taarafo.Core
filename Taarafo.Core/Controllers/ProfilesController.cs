42c115843d1b11c9c4a84c8a2e8ae64613905d2d
GulchexrasGithub <burgulchexra@gmail.com>
2022-12-26 02:40:40 +0500

    CONTROLLERS: GET All Profiles

--- Taarafo.Core/Controllers/ProfilesController.cs
+++ Taarafo.Core/Controllers/ProfilesController.cs
@@ -3,6 +3,7 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
@@ -12,47 +13,67 @@ using Taarafo.Core.Services.Foundations.Profiles;
 
 namespace Taarafo.Core.Controllers
 {
-	[ApiController]
-	[Route("api/[controller]")]
-	public class ProfilesController : RESTFulController
-	{
-		private readonly IProfileService profileService;
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfilesController : RESTFulController
+    {
+        private readonly IProfileService profileService;
 
-		public ProfilesController(IProfileService profileService) =>
-			this.profileService = profileService;
+        public ProfilesController(IProfileService profileService) =>
+            this.profileService = profileService;
 
-		[HttpPost]
-		public async ValueTask<ActionResult<Profile>> PostProfileAsync(Profile profile)
-		{
-			try
-			{
-				Profile addedProfile =
-					await this.profileService.AddProfileAsync(profile);
+        [HttpPost]
+        public async ValueTask<ActionResult<Profile>> PostProfileAsync(Profile profile)
+        {
+            try
+            {
+                Profile addedProfile =
+                    await this.profileService.AddProfileAsync(profile);
 
-				return Created(addedProfile);
-			}
-			catch (ProfileValidationException profileValidationException)
-			{
-				return BadRequest(profileValidationException.InnerException);
-			}
-			catch (ProfileDependencyValidationException profileDependencyValidationException)
-				when (profileDependencyValidationException.InnerException is InvalidProfileReferenceException)
-			{
-				return FailedDependency(profileDependencyValidationException.InnerException);
-			}
-			catch (ProfileDependencyValidationException profileDependencyValidationException)
-				when (profileDependencyValidationException.InnerException is AlreadyExistsProfileException)
-			{
-				return Conflict(profileDependencyValidationException.InnerException);
-			}
-			catch (ProfileDependencyException profileDependencyException)
-			{
-				return InternalServerError(profileDependencyException);
-			}
-			catch (ProfileServiceException profileServiceException)
-			{
-				return InternalServerError(profileServiceException);
-			}
-		}
-	}
+                return Created(addedProfile);
+            }
+            catch (ProfileValidationException profileValidationException)
+            {
+                return BadRequest(profileValidationException.InnerException);
+            }
+            catch (ProfileDependencyValidationException profileDependencyValidationException)
+                when (profileDependencyValidationException.InnerException is InvalidProfileReferenceException)
+            {
+                return FailedDependency(profileDependencyValidationException.InnerException);
+            }
+            catch (ProfileDependencyValidationException profileDependencyValidationException)
+                when (profileDependencyValidationException.InnerException is AlreadyExistsProfileException)
+            {
+                return Conflict(profileDependencyValidationException.InnerException);
+            }
+            catch (ProfileDependencyException profileDependencyException)
+            {
+                return InternalServerError(profileDependencyException);
+            }
+            catch (ProfileServiceException profileServiceException)
+            {
+                return InternalServerError(profileServiceException);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<IQueryable<Profile>> GetAllProfiless()
+        {
+            try
+            {
+                IQueryable<Profile> retrievedProfiles =
+                    this.profileService.RetrieveAllProfiles();
+
+                return Ok(retrievedProfiles);
+            }
+            catch (ProfileDependencyException profileDependencyException)
+            {
+                return InternalServerError(profileDependencyException);
+            }
+            catch (ProfileServiceException profileServiceException)
+            {
+                return InternalServerError(profileServiceException);
+            }
+        }
+    }
 }
