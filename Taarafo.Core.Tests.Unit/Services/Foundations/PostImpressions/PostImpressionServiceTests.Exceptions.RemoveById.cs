4bc1dc4872273045429583cddcb05f7b22dbe033
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-12-26 19:02:24 +0300

    ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync ->PASS

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/PostImpressions/PostImpressionServiceTests.Exceptions.RemoveById.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Taarafo.Core.Models.PostImpressions;
+using Taarafo.Core.Models.PostImpressions.Exceptions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.PostImpressions
+{
+    public partial class PostImpressionServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync()
+        {
+            //given
+            Guid somePostId = Guid.NewGuid();
+            Guid someProfileId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedPostImpressionStorageException =
+                new FailedPostImpressionStorageException(sqlException);
+
+            var expectedPostImpressionDependencyException =
+                new PostImpressionDependencyException(failedPostImpressionStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectPostImpressionByIdsAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<Guid>())).ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<PostImpression> deletePostImpressionTask =
+                this.postImpressionService.RemovePostImpressionByIdAsync(somePostId, someProfileId);
+
+            PostImpressionDependencyException actualPostImpressionDependencyException =
+                await Assert.ThrowsAsync<PostImpressionDependencyException>(
+                    deletePostImpressionTask.AsTask);
+
+            //then
+            actualPostImpressionDependencyException.Should().BeEquivalentTo(
+                expectedPostImpressionDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPostImpressionByIdsAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedPostImpressionDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
