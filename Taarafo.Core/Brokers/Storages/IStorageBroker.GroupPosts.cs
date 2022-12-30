27a9cd0d63eace2e85226eb83879c0662d9a4116
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-30 12:06:19 -0800

    BROKERS: Select All Group Posts

--- Taarafo.Core/Brokers/Storages/IStorageBroker.GroupPosts.cs
+++ Taarafo.Core/Brokers/Storages/IStorageBroker.GroupPosts.cs
@@ -3,14 +3,16 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System.Linq;
 using System.Threading.Tasks;
 using Taarafo.Core.Models.GroupPosts;
 
 namespace Taarafo.Core.Brokers.Storages
 {
-	public partial interface IStorageBroker
-	{
-		ValueTask<GroupPost> InsertGroupPostAsync(GroupPost groupPost);
-		ValueTask<GroupPost> DeleteGroupPostAsync(GroupPost groupPost);
-	}
+    public partial interface IStorageBroker
+    {
+        ValueTask<GroupPost> InsertGroupPostAsync(GroupPost groupPost);
+        IQueryable<GroupPost> SelectAllGroupPosts();
+        ValueTask<GroupPost> DeleteGroupPostAsync(GroupPost groupPost);
+    }
 }
