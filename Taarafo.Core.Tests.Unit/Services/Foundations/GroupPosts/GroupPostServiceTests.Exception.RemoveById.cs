9e01aba23b3c7a62200e0c44ffa4d9d762fac4cb
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-31 09:50:54 -0800

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/GroupPosts/GroupPostServiceTests.Exception.RemoveById.cs
@@ -0,0 +1,68 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Taarafo.Core.Models.GroupPosts;
+using Taarafo.Core.Models.GroupPosts.Exceptions;
+using Tarteeb.Api.Models.Teams.Exceptions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.GroupPosts
+{
+    public partial class GroupPostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someGroupPostId = Guid.NewGuid();
+
+            var databaseUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedGroupPostException =
+                new LockedGroupPostException(databaseUpdateConcurrencyException);
+
+            var expectedGroupPostDependencyValidationException =
+                new GroupPostDependencyValidationException(lockedGroupPostException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGroupPostByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<GroupPost> removeGroupPostByIdTask =
+                this.groupPostService.RemoveGroupPostByIdAsync(someGroupPostId);
+
+            GroupPostDependencyValidationException actualGroupPostDependencyValidationException =
+                await Assert.ThrowsAsync<GroupPostDependencyValidationException>(
+                    removeGroupPostByIdTask.AsTask);
+
+            // then
+            actualGroupPostDependencyValidationException.Should().BeEquivalentTo(
+                expectedGroupPostDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGroupPostByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGroupPostDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteGroupPostAsync(It.IsAny<GroupPost>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Taarafo.Core/Models/GroupPosts/Exceptions/LockedGroupPostException.cs
@@ -0,0 +1,17 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using Xeptions;
+
+namespace Tarteeb.Api.Models.Teams.Exceptions
+{
+    public class LockedGroupPostException : Xeption
+    {
+        public LockedGroupPostException(Exception innerException)
+            : base(message: "GrouPost is locked, please try again.", innerException)
+        { }
+    }
+}
