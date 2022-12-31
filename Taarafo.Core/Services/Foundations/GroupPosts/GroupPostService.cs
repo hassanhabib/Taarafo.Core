fd654671f470005798a3c7247d50c914c64199a2
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-31 08:41:24 -0800

    CODE RUB: Fix indentation

--- Taarafo.Core/Services/Foundations/GroupPosts/GroupPostService.cs
+++ Taarafo.Core/Services/Foundations/GroupPosts/GroupPostService.cs
@@ -32,6 +32,6 @@ namespace Taarafo.Core.Services.Foundations.GroupPosts
         });
 
         public IQueryable<GroupPost> RetrieveAllGroupPosts() =>
-         TryCatch(() => this.storageBroker.SelectAllGroupPosts());
+        TryCatch(() => this.storageBroker.SelectAllGroupPosts());
     }
 }
