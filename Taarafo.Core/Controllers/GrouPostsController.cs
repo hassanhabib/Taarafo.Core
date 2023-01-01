1f6c94bbab2d807a23a22cf8f812a6a5a3241538
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-01-01 03:41:14 -0800

    EXPOSERS: Post Group Post

---
+++ Taarafo.Core/Controllers/GrouPostsController.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using Taarafo.Core.Models.GroupPosts;
+using Taarafo.Core.Models.GroupPosts.Exceptions;
+using Taarafo.Core.Services.Foundations.GroupPosts;
+
+namespace Taarafo.Core.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GrouPostsController : RESTFulController
+    {
+        private readonly IGroupPostService groupPostService;
+
+        public GrouPostsController(IGroupPostService groupPostService) =>
+            this.groupPostService = groupPostService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<GroupPost>> PostGroupPostAsync(GroupPost groupPost)
+        {
+            try
+            {
+                GroupPost addedGroupPost =
+                    await this.groupPostService.AddGroupPostAsync(groupPost);
+
+                return Created(addedGroupPost);
+            }
+            catch (GroupPostValidationException groupPostValidationException)
+            {
+                return BadRequest(groupPostValidationException.InnerException);
+            }
+            catch (GroupPostDependencyValidationException groupPostDependencyValidationException)
+               when (groupPostDependencyValidationException.InnerException is AlreadyExistsGroupPostException)
+            {
+                return Conflict(groupPostDependencyValidationException.InnerException);
+            }
+            catch (GroupPostDependencyException groupPostDependencyException)
+            {
+                return InternalServerError(groupPostDependencyException);
+            }
+            catch (GroupPostServiceException groupPostServiceException)
+            {
+                return InternalServerError(groupPostServiceException);
+            }
+        }
+    }
+}
