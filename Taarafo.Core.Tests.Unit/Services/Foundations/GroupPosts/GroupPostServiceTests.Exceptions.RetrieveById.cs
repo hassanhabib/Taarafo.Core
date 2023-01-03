10ec5cfb668556059782ffecde7ef0c82b0a65bf
GulchexrasGithub <burgulchexra@gmail.com>
2023-01-04 00:20:39 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/GroupPosts/GroupPostServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,63 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Taarafo.Core.Models.GroupPosts;
+using Taarafo.Core.Models.GroupPosts.Exceptions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.GroupPosts
+{
+    public partial class GroupPostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            //given
+            Guid someGroupId = Guid.NewGuid();
+            Guid somePostId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedGroupPostStorageException =
+                new FailedGroupPostStorageException(sqlException);
+
+            var expectedGroupPostDependencyException =
+                new GroupPostDependencyException(failedGroupPostStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGroupPostByIdAsync(someGroupId, somePostId))
+                    .ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<GroupPost> retrieveGroupPostByIdTask =
+                this.groupPostService.RetrieveGroupPostByIdAsync(someGroupId, somePostId);
+
+            GroupPostDependencyException actualGroupPostDependencyException =
+                await Assert.ThrowsAsync<GroupPostDependencyException>(
+                    retrieveGroupPostByIdTask.AsTask);
+
+            //then
+            actualGroupPostDependencyException.Should().BeEquivalentTo(
+                   expectedGroupPostDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGroupPostByIdAsync(It.IsAny<Guid>(), (It.IsAny<Guid>())),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedGroupPostDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
