6b984da62f66fa353308b0fba002961002f148bc
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-01-03 15:09:16 +0300

    CODE RUB: Fix Formatting

--- Taarafo.Core/Brokers/Storages/IStorageBroker.PostReports.cs
+++ Taarafo.Core/Brokers/Storages/IStorageBroker.PostReports.cs
@@ -4,7 +4,6 @@
 // ---------------------------------------------------------------
 
 using System;
-using System.Threading.Tasks;
 using System.Linq;
 using System.Threading.Tasks;
 using Taarafo.Core.Models.PostReports;
--- Taarafo.Core/Brokers/Storages/StorageBroker.PostReports.cs
+++ Taarafo.Core/Brokers/Storages/StorageBroker.PostReports.cs
@@ -27,7 +27,7 @@ namespace Taarafo.Core.Brokers.Storages
         public async ValueTask<PostReport> UpdatePostReportAsync(PostReport postReport) =>
             await UpdateAsync(postReport);
 
-        public async ValueTask<PostReport> DeletePostReportAsync(PostReport postReport)=>
+        public async ValueTask<PostReport> DeletePostReportAsync(PostReport postReport) =>
             await DeleteAsync(postReport);
     }
 }
