ae0efb9d412c7e84fe8ee46cbbf9eb1ee482120b
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-01-04 01:38:57 -0800

    ShouldThrowValidationExceptionOnModifyIfGroupPostIsNullAndLogItAsync -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/GroupPosts/GroupPostServiceTests.Validations.Modify.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Taarafo.Core.Models.GroupPosts;
+using Taarafo.Core.Models.GroupPosts.Exceptions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.GroupPosts
+{
+    public partial class GroupPostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfGroupPostIsNullAndLogItAsync()
+        {
+            // given
+            GroupPost nullGroupPost = null;
+            var nullGroupPostException = new NullGroupPostException();
+
+            var expectedGroupPostValidationException =
+                new GroupPostValidationException(nullGroupPostException);
+
+            // when
+            ValueTask<GroupPost> modifyGroupPostTask =
+                this.groupPostService.ModifyGroupPostAsync(nullGroupPost);
+
+            GroupPostValidationException actualGroupPostValidationException =
+                await Assert.ThrowsAsync<GroupPostValidationException>(
+                    modifyGroupPostTask.AsTask);
+
+            // then
+            actualGroupPostValidationException.Should().BeEquivalentTo(expectedGroupPostValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGroupPostValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateGroupPostAsync(It.IsAny<GroupPost>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
