1e7aa9ab6497ca519bbb534c9dc4c2a18bcd6e43
Muhammad Muso <musogulomov123@gmail.com>
2023-01-09 21:03:07 +0500

    EXPOSERS: Post Group

---
+++ Taarafo.Core/Controllers/GroupsController.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using Taarafo.Core.Models.Groups;
+using Taarafo.Core.Models.Groups.Exceptions;
+using Taarafo.Core.Services.Foundations.Groups;
+
+namespace Taarafo.Core.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GroupsController : RESTFulController
+    {
+        private readonly IGroupService groupService;
+
+        public GroupsController(IGroupService groupService) =>
+            this.groupService = groupService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Group>> GroupPostAsync(Group group)
+        {
+            try
+            {
+                Group addedGroup =
+                    await this.groupService.AddGroupAsync(group);
+
+                return Created(addedGroup);
+            }
+            catch (GroupValidationException groupValidationException)
+            {
+                return BadRequest(groupValidationException.InnerException);
+            }
+            catch (GroupDependencyException groupDependencyException)
+                when (groupDependencyException.InnerException is InvalidGroupReferenceException)
+            {
+                return FailedDependency(groupDependencyException.InnerException);
+            }
+            catch (GroupDependencyException groupDependencyException)
+                when (groupDependencyException.InnerException is AlreadyExistsGroupException)
+            {
+                return Conflict(groupDependencyException.InnerException);
+            }
+            catch (GroupDependencyException groupDependencyException)
+            {
+                return InternalServerError(groupDependencyException);
+            }
+            catch (GroupServiceException groupServiceException)
+            {
+                return InternalServerError(groupServiceException);
+            }
+        }
+    }
+}
