01a1d662bfffe925253115ecef86af6956a5e211
GulchexrasGithub <burgulchexra@gmail.com>
2023-01-09 21:18:31 +0500

    PROCESSING: Upsert PostImpression

---
+++ Taarafo.Core/Services/Processings/PostImpressions/IPostImpressionProcessingService.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Taarafo.Core.Models.PostImpressions;
+
+namespace Taarafo.Core.Services.Processings.PostImpressions
+{
+    public interface IPostImpressionProcessingService
+    {
+        ValueTask<PostImpression> UpsertPostImpressionAsync(PostImpression postImpression);
+    }
+}
---
+++ Taarafo.Core/Services/Processings/PostImpressions/PostImpressionProcessingService.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Taarafo.Core.Brokers.Loggings;
+using Taarafo.Core.Models.PostImpressions;
+using Taarafo.Core.Services.Foundations.PostImpressions;
+
+namespace Taarafo.Core.Services.Processings.PostImpressions
+{
+    public class PostImpressionProcessingService : IPostImpressionProcessingService
+    {
+        private readonly IPostImpressionService postImpressionService;
+        private readonly ILoggingBroker loggingBroker;
+
+        public PostImpressionProcessingService(
+            IPostImpressionService postImpressionService,
+            ILoggingBroker loggingBroker)
+        {
+            this.postImpressionService = postImpressionService;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<PostImpression> UpsertPostImpressionAsync(PostImpression postImpression) =>
+            throw new System.NotImplementedException();
+
+    }
+}
