bbc189fc71be9b5c804b95ea062c004d81130ece
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-01-10 10:53:33 +0300

    ShouldRetrieveAllPostImpressions -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Processings/PostImpressions/PostImpressionProcessingServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Taarafo.Core.Models.PostImpressions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Processings.PostImpressions
+{
+    public partial class PostImpressionProcessingServiceTests
+    {
+        [Fact]
+        public void ShouldRetrieveAllPostImpressions()
+        {
+            //given
+            IQueryable<PostImpression> randomPostImpressions = CreateRandomPostImpressions();
+            IQueryable<PostImpression> storagePostImpressions = randomPostImpressions;
+            IQueryable<PostImpression> expectedPostImpressions = storagePostImpressions;
+
+            this.postImpressionServiceMock.Setup(service=>
+                service.RetrieveAllPostImpressions())
+                    .Returns(expectedPostImpressions);
+
+            //when
+            IQueryable<PostImpression> actualPostImpression=
+                this.postImpressionProcessingService.RetrieveAllPostImpressionsAsync();
+
+            //then
+            actualPostImpression.Should().BeEquivalentTo(expectedPostImpressions);
+
+            this.postImpressionServiceMock.Verify(service =>
+                service.RetrieveAllPostImpressions(), Times.Once);
+
+            this.postImpressionServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Taarafo.Core.Tests.Unit/Services/Processings/PostImpressions/PostimpressionProcessingServiceTests.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using Moq;
+using Taarafo.Core.Brokers.Loggings;
+using Taarafo.Core.Models.PostImpressions;
+using Taarafo.Core.Services.Foundations.PostImpressions;
+using Taarafo.Core.Services.Processings.PostImpressions;
+using Tynamix.ObjectFiller;
+
+namespace Taarafo.Core.Tests.Unit.Services.Processings.PostImpressions
+{
+    public partial class PostImpressionProcessingServiceTests
+    {
+        private readonly Mock<IPostImpressionService> postImpressionServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IPostImpressionProcessingService postImpressionProcessingService;
+
+        public PostImpressionProcessingServiceTests()
+        {
+            this.postImpressionServiceMock = new Mock<IPostImpressionService>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.postImpressionProcessingService = new PostImpressionProcessingService(
+                postImpressionService: this.postImpressionServiceMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private IQueryable<PostImpression> CreateRandomPostImpressions() =>
+            CreatePostImpressionFiller().Create(count: GetRandomNumber()).AsQueryable();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset()=>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Filler<PostImpression> CreatePostImpressionFiller()
+        {
+            var filler = new Filler<PostImpression>();
+            filler.Setup().OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset());
+
+            return filler;
+        }
+    }
+}
