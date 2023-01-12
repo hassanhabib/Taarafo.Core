aa54c89ee4cd17cc88689726c58e3127020e7e23
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-01-12 22:14:09 +0300

    CODE RUB : Fix Formatting

--- Taarafo.Core/Models/Profiles/Profile.cs
+++ Taarafo.Core/Models/Profiles/Profile.cs
@@ -9,15 +9,15 @@ using Taarafo.Core.Models.PostImpressions;
 
 namespace Taarafo.Core.Models.Profiles
 {
-	public class Profile
-	{
-		public Guid Id { get; set; }
-		public string Username { get; set; }
-		public string Name { get; set; }
-		public string Email { get; set; }
-		public string Avatar { get; set; }
-		public DateTimeOffset CreatedDate { get; set; }
-		public DateTimeOffset UpdatedDate { get; set; }
-		public IEnumerable<PostImpression> PostImpressions { get; set; }
-	}
+    public class Profile
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Avatar { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+        public IEnumerable<PostImpression> PostImpressions { get; set; }
+    }
 }
