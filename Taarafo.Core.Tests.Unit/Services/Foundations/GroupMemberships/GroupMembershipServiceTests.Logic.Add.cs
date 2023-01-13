3aaf009f2c98a8e844405c1ab2c13209e962b09e
GulchexrasGithub <burgulchexra@gmail.com>
2023-01-14 01:42:18 +0500

    ShouldAddGroupMembershipAsync -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/GroupMemberships/GroupMembershipServiceTests.Logic.Add.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Taarafo.Core.Models.GroupMemberships;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.GroupMemberships
+{
+    public partial class GroupMembershipServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddGroupMembershipAsync()
+        {
+            // given
+            GroupMembership randomGroupMembership = CreateRandomGroupMembership();
+            GroupMembership inputGroupMembership = randomGroupMembership;
+            GroupMembership storageGroupMembership = inputGroupMembership;
+            GroupMembership expectedGroupMembership = storageGroupMembership.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertGroupMembershipAsync(inputGroupMembership))
+                    .ReturnsAsync(storageGroupMembership);
+
+            // when
+            GroupMembership actualGroupMembership =
+                await this.groupMembershipService.AddGroupMembershipAsync(inputGroupMembership);
+
+            // then
+            actualGroupMembership.Should().BeEquivalentTo(expectedGroupMembership);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertGroupMembershipAsync(inputGroupMembership),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/GroupMemberships/GroupMembershipServiceTests.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Moq;
+using Taarafo.Core.Brokers.DateTimes;
+using Taarafo.Core.Brokers.Loggings;
+using Taarafo.Core.Brokers.Storages;
+using Taarafo.Core.Models.GroupMemberships;
+using Taarafo.Core.Services.Foundations.GroupMemberships;
+using Tynamix.ObjectFiller;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.GroupMemberships
+{
+    public partial class GroupMembershipServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IGroupMembershipService groupMembershipService;
+
+        public GroupMembershipServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.groupMembershipService = new GroupMembershipService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static GroupMembership CreateRandomGroupMembership() =>
+            CreateGroupMembershipFiller(GetRandomDateTimeOffset()).Create();
+
+        private static Filler<GroupMembership> CreateGroupMembershipFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<GroupMembership>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
