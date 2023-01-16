626261f2e684130c21567c711189869db8e987fd
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-01-16 07:56:55 -0800

    ShouldRetrieveAllEvents -> Fail

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Events/EventServiceTests.cs
@@ -0,0 +1,62 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using Moq;
+using Taarafo.Core.Brokers.DateTimes;
+using Taarafo.Core.Brokers.Loggings;
+using Taarafo.Core.Brokers.Storages;
+using Taarafo.Core.Models.Events;
+using Taarafo.Core.Services.Foundations.Events;
+using Tynamix.ObjectFiller;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.Events
+{
+    public partial class EventServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IEventService eventService;
+
+        public EventServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.eventService = new EventService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static Event CreateRandomEvent(DateTimeOffset dates) =>
+            CreateEventFiller(dates).Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static int GetRandomNumber() =>
+           new IntRange(min: 2, max: 99).GetValue();
+
+        private static IQueryable<Event> CreateRandomEvents()
+        {
+            return CreateEventFiller(dates: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber()).AsQueryable();
+        }
+
+        private static Filler<Event> CreateEventFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Event>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
---
+++ Taarafo.Core/Services/Foundations/Events/EventService.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Linq;
+using Taarafo.Core.Brokers.DateTimes;
+using Taarafo.Core.Brokers.Loggings;
+using Taarafo.Core.Brokers.Storages;
+using Taarafo.Core.Models.Events;
+
+namespace Taarafo.Core.Services.Foundations.Events
+{
+    public partial class EventService : IEventService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public EventService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public IQueryable<Event> RetrieveAllEvents()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
