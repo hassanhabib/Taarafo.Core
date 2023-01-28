a1cdebad85ef8894125b2d41577d0c3445de3db5
Muhammad Muso <musogulomov123@gmail.com>
2023-01-28 23:59:31 +0500

    FOUNDATIONS: Add PostReport

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/PostReports/PostReportServiceTests.Logic.Add.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Taarafo.Core.Models.PostReports;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.PostReports
+{
+    public partial class PostReportServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddPostReportAsync()
+        {
+            // given
+            DateTimeOffset randomDateTime = GetRandomDateTimeOffset();
+            PostReport randomPostReport = CreateRandomPostReport(randomDateTime);
+            PostReport inputPostReport = randomPostReport;
+            PostReport storagePostReport = inputPostReport;
+            PostReport expectedPostReport = storagePostReport.DeepClone();
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Returns(randomDateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertPostReportAsync(inputPostReport))
+                    .ReturnsAsync(storagePostReport);
+
+            // when
+            PostReport actualPostReport =
+                await this.postReportService.AddPostReportAsync(inputPostReport);
+
+            // then
+            actualPostReport.Should().BeEquivalentTo(expectedPostReport);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertPostReportAsync(inputPostReport), Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Taarafo.Core/Services/Foundations/PostReports/PostReportService.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Taarafo.Core.Brokers.DateTimes;
+using Taarafo.Core.Brokers.Loggings;
+using Taarafo.Core.Brokers.Storages;
+using Taarafo.Core.Models.PostReports;
+
+namespace Taarafo.Core.Services.Foundations.PostReports
+{
+    public class PostReportService : IPostReportService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public PostReportService(IStorageBroker storageBroker, ILoggingBroker loggingBroker, IDateTimeBroker dateTimeBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
+        }
+
+        public ValueTask<PostReport> AddPostReportAsync(PostReport postReport) =>
+            throw new NotImplementedException();
+    }
+}
