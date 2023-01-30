7db8a0eb65708bf9b32ad7e254ec9e452c04637b
Muhammad Muso <musogulomov123@gmail.com>
2023-01-30 14:38:26 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync-> PASS

--- Taarafo.Core/Services/Foundations/PostReports/PostReportService.Exceptions.cs
+++ Taarafo.Core/Services/Foundations/PostReports/PostReportService.Exceptions.cs
@@ -4,6 +4,7 @@
 // ---------------------------------------------------------------
 
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using Taarafo.Core.Models.PostReports;
 using Taarafo.Core.Models.PostReports.Exceptions;
 using Xeptions;
@@ -28,6 +29,12 @@ namespace Taarafo.Core.Services.Foundations.PostReports
             {
                 throw CreateAndLogValidationException(invalidPostReportException);
             }
+            catch (SqlException sqlException)
+            {
+                var failedPostReportStorageException = new FailedPostReportStorageException(sqlException);
+
+                throw CreateAndLogCriticalDependencyException(failedPostReportStorageException);
+            }
         }
 
         private PostReportValidationException CreateAndLogValidationException(Xeption exception)
@@ -38,6 +45,14 @@ namespace Taarafo.Core.Services.Foundations.PostReports
             this.loggingBroker.LogError(postReportValidationException);
 
             return postReportValidationException;
+        }
+
+        private PostReportDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var postReportDependencyException = new PostReportDependencyException(exception);
+            this.loggingBroker.LogCritical(postReportDependencyException);
+
+            return postReportDependencyException;
         }
     }
 }
