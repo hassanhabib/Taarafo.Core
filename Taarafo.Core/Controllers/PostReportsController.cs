49e755408797b8f51e33697f017e7c7f812c3f28
GulchexrasGithub <burgulchexra@gmail.com>
2023-03-09 02:30:52 +0500

    CONTROLLERS: GET ALL PostReports

---
+++ Taarafo.Core/Controllers/PostReportsController.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using Taarafo.Core.Models.PostReports;
+using Taarafo.Core.Models.PostReports.Exceptions;
+using Taarafo.Core.Services.Foundations.PostReports;
+
+namespace Taarafo.Core.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostReportsController : RESTFulController
+    {
+        private readonly IPostReportService postReportService;
+
+        public PostReportsController(IPostReportService postReportService) =>
+            this.postReportService = postReportService;
+
+        [HttpGet]
+        public ActionResult<IQueryable<PostReport>> GetAllPostReports()
+        {
+            try
+            {
+                IQueryable<PostReport> retrievedPostReports =
+                    this.postReportService.RetrieveAllPostReports();
+
+                return Ok(retrievedPostReports);
+            }
+            catch (PostReportDependencyException postReportDependencyException)
+            {
+                return InternalServerError(postReportDependencyException.InnerException);
+            }
+            catch (PostReportServiceException postReportServiceException)
+            {
+                return InternalServerError(postReportServiceException.InnerException);
+            }
+        }
+    }
+}
