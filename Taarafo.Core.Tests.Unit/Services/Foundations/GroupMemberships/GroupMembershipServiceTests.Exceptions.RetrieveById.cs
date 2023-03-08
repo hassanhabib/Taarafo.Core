98f595a7c039447d8da3742e42672b860adb334f
GulchexrasGithub <burgulchexra@gmail.com>
2023-03-08 22:54:46 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/GroupMemberships/GroupMembershipServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,62 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Taarafo.Core.Models.GroupMemberships;
+using Taarafo.Core.Models.GroupMemberships.Exceptions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.GroupMemberships
+{
+    public partial class GroupMembershipServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedGroupMembershipStorageException =
+                new FailedGroupMembershipStorageException(sqlException);
+
+            var expectedGroupMembershipDependencyException =
+                new GroupMembershipDependencyException(failedGroupMembershipStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGroupMembershipByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<GroupMembership> retrieveGroupMembershipByIdTask =
+                this.groupMembershipService.RetrieveGroupMembershipByIdAsync(someId);
+
+            GroupMembershipDependencyException actualGroupMembershipDependencyException =
+                await Assert.ThrowsAsync<GroupMembershipDependencyException>(
+                    retrieveGroupMembershipByIdTask.AsTask);
+
+            // then
+            actualGroupMembershipDependencyException.Should().BeEquivalentTo(
+                expectedGroupMembershipDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGroupMembershipByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedGroupMembershipDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
