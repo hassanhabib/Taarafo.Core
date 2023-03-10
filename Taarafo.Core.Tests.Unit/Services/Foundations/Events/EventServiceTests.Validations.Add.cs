d251a0b2e5dadf6ca752f6ed3a868474e65c2f69
Ricardo Cruz <rcruzfreelance@outlook.com>
2023-03-10 17:01:10 -0400

    ShouldThrowValidationExceptionOnAddIfEventIsNullAndLogitAsync -> FAIL

---
+++ Taarafo.Core.Tests.Unit/Services/Foundations/Events/EventServiceTests.Validations.Add.cs
@@ -0,0 +1,51 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Taarafo.Core.Models.Events;
+using Taarafo.Core.Models.Events.Exceptions;
+using Xunit;
+
+namespace Taarafo.Core.Tests.Unit.Services.Foundations.Events
+{
+    public partial class EventServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfEventIsNullAndLogitAsync()
+        {
+             // given
+            Event nullEvent = null;
+
+            var nullEventException =
+                new NullEventException();
+
+            var expectedEventValidationException =
+                new EventValidationException(nullEventException);
+
+            // when
+            ValueTask<Event> addEventTask =
+                this.eventService.AddEventAsync(nullEvent);
+
+            EventValidationException actualEventValidationException =
+                await Assert.ThrowsAsync<EventValidationException>(
+                    addEventTask.AsTask);
+
+            // then
+            actualEventValidationException.Should().BeEquivalentTo(
+                expectedEventValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedEventValidationException))),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
